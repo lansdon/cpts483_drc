a72d0a5423fb7bcd2e0b5217f21c1c0d86a57e7e
Lansdon Page <lansdon.page@email.wsu.edu>
2014-06-02 00:06:11 -0700

    Re-organizing MediatorKeys.h by event chain.

--- drc/drc_shared/mediator/MediatorKeys.h
+++ drc/drc_shared/mediator/MediatorKeys.h
@@ -11,53 +11,53 @@
 
 #include <string>
 
+//////////////////////////////////////////////////////////////////
+//				EVENTS INFORMATION
+// Events are chained together between the components of the system (gui, db, bl)
+// They are being organized in this document by a single event, with the chain shown in order.
+// Please follow this convention for clarity and ease of debugging.
+//////////////////////////////////////////////////////////////////
+
 
 //////////////////////////////////////////////////////////////////
-//				GUI EVENTS
-// Everyting in this section is called by the GUI
-// For example, when an intake form "submit" button on the gui
-// is pressed, an event will be called which contains the form
-// data as an argument. The BL will register to recieve that event.
+//				AUTHENTICATE USER
+// When the user hit's login - we must see if they entered correct username + password
 //////////////////////////////////////////////////////////////////                          ARGUMENT TYPE
-const std::string MKEY_GUI_SUBMIT_FRUIT_NAME = "MKEY_GUI_SUBMIT_FRUIT_NAME";                // std::string*
-
-const std::string MKEY_GUI_SEARCH_FOR_USERNAME = "MKEY_GUI_SEARCH_FOR_USERNAME";           // std::string*
-
-const std::string MKEY_GUI_SUBMIT_INTAKE_FORM = "MKEY_GUI_SUBMIT_INTAKE_FORM";              // Intake*
-const std::string MKEY_GUI_LOAD_INTAKE_FORM = "MKEY_GUI_LOAD_INTAKE_FORM";                // Intake*
-
 const std::string MKEY_GUI_AUTHENTICATE_USER = "MKEY_GUI_AUTHENTICATE_USER";                // User*
-
+const std::string MKEY_BL_AUTHENTICATE_USER_DONE = "MKEY_BL_AUTHENTICATE_USER_DONE";        // User*
+const std::string MKEY_DB_AUTHENTICATE_USER_DONE = "MKEY_DB_AUTHENTICATE_USER_DONE";        // std::vector<User*>*
 
 //////////////////////////////////////////////////////////////////
-//				BUSINESS LOGIC EVENTS
-// Everyting in this section is called by the BUSINESS LOGIC
-// After the business logic validates an intake form
-// it will fire an event containing the form data as an argument
-// which the database has registered to recieve.
+//				SUBMIT FRUIT NAME
+// Test function to save a fruit in the database
 //////////////////////////////////////////////////////////////////                          ARGUMENT TYPE
-const std::string MKEY_BL_VALIDATE_FRUITNAME_DONE = "MKEY_BL_VALIDATE_FRUITNAME_DONE";       // std::string*
+const std::string MKEY_GUI_SUBMIT_FRUIT_NAME = "MKEY_GUI_SUBMIT_FRUIT_NAME";                // Fruit*
+const std::string MKEY_BL_VALIDATE_FRUITNAME_DONE = "MKEY_BL_VALIDATE_FRUITNAME_DONE";      // Fruit*
+const std::string MKEY_DB_PERSIST_FRUIT_NAME_DONE = "MKEY_DB_PERSIST_FRUIT_NAME_DONE";      // Fruit
 
+//////////////////////////////////////////////////////////////////
+//				SEARCH FOR USER BY NAME ????? Test Function ???
+// Test function to save a fruit in the database
+//////////////////////////////////////////////////////////////////                          ARGUMENT TYPE
+const std::string MKEY_GUI_SEARCH_FOR_USERNAME = "MKEY_GUI_SEARCH_FOR_USERNAME";           // std::string*
 const std::string MKEY_BL_RETURN_SEARCH_RESULTS = "MKEY_BL_RETURN_SEARCH_RESULTS";          // class Intake*
 
+//////////////////////////////////////////////////////////////////
+//				SAVE INTAKE FORM
+// Save an intake form to the database
+//////////////////////////////////////////////////////////////////                          ARGUMENT TYPE
+const std::string MKEY_GUI_SUBMIT_INTAKE_FORM = "MKEY_GUI_SUBMIT_INTAKE_FORM";              // Intake*
 const std::string MKEY_BL_VALIDATE_SAVE_INTAKE_FORM_DONE = "MKEY_BL_VALIDATE_SAVE_INTAKE_FORM_DONE";   // Intake*
-const std::string MKEY_BL_VALIDATE_LOAD_INTAKE_FORM_DONE = "MKEY_BL_VALIDATE_LOAD_INTAKE_FORM_DONE";   // Intake*
-
-const std::string MKEY_BL_AUTHENTICATE_USER_DONE = "MKEY_BL_AUTHENTICATE_USER_DONE";   // Intake*
-
-
+const std::string MKEY_DB_PERSIST_INTAKE_FORM_DONE = "MKEY_DB_PERSIST_INTAKE_FORM_DONE";      // Intake*
 
 //////////////////////////////////////////////////////////////////
-//				DB EVENTS
-// Everyting in this section is called by the  DB
-// For example, when an intake form is submitted to be
-// persisted in the DB and DB is done. return results
-// as an event for anyone listening to recieve.
+//				LOAD INTAKE FORM
+// Load an intake form from the database
 //////////////////////////////////////////////////////////////////                          ARGUMENT TYPE
-const std::string MKEY_DB_PERSIST_FRUIT_NAME_DONE = "MKEY_DB_PERSIST_FRUIT_NAME_DONE";       // nil
-const std::string MKEY_DB_PERSIST_INTAKE_FORM_DONE = "MKEY_DB_PERSIST_INTAKE_FORM_DONE";      // Intake*
+const std::string MKEY_GUI_LOAD_INTAKE_FORM = "MKEY_GUI_LOAD_INTAKE_FORM";                // Intake*
+const std::string MKEY_BL_VALIDATE_LOAD_INTAKE_FORM_DONE = "MKEY_BL_VALIDATE_LOAD_INTAKE_FORM_DONE";   // Intake*
 const std::string MKEY_DB_LOAD_INTAKE_FORM_DONE = "MKEY_DB_LOAD_INTAKE_FORM_DONE";      // std::vector<Intake*>*
-const std::string MKEY_DB_AUTHENTICATE_USER_DONE = "MKEY_DB_AUTHENTICATE_USER_DONE";      // std::vector<Intake*>*
+
 
 
 #endif
