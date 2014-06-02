7cb6e6c33c06cc49dc5f13d89262e2339b0507f2
Tweety79rw <ryan.wilson2@email.wsu.edu>
2014-06-02 16:05:01 -0700

    Wreorder problems with asyncmediatorworker

--- drc/drc_shared/mediator/asyncmediatorworker.h
+++ drc/drc_shared/mediator/asyncmediatorworker.h
@@ -4,6 +4,7 @@
 #include <QObject>
 #include "MediatorArg.h"
 #include "Mediator.h"
+#include <ctime>
 
 const unsigned long DEF_ASYNC_TIMEOUT_SECS = 10; // timeout in seconds
 
@@ -24,25 +25,25 @@ signals:
     void error(QString err);
 
 private:
-    unsigned long _timeoutSecs;			// How long before we stop waiting for a response
-    bool _willWaitForResponse;          // Turn this on when you want to handle a response event.
+
+
     bool _waiting;						// This will block additional calls to Send() if a Send is already in progress.
     bool WaitForResponse();             // The loop handles timeouts while waiting
-
-    MediatorArg _sendMediatorArg;			// This is the argument being sent.
-    MediatorArg _recieveMediatorArg;        // This is the argument being recieved.
-
-    MediatorCallbackFunc _callback;			// This is called when a response is received.
 
     std::string _sendEventMediatorKey;		// Send Event Key
     std::string _recieveEventMediatorKey;	// Listen for this result Key
 
+    MediatorCallbackFunc _callback;			// This is called when a response is received.
+
+
+    MediatorArg _sendMediatorArg;			// This is the argument being sent.
+    MediatorArg _recieveMediatorArg;        // This is the argument being recieved.
     // Internal mediator callbacks
     MediatorId ResponseRecievedId;
 
     double Elapsed(clock_t startTime);
-
+    bool _willWaitForResponse;          // Turn this on when you want to handle a response event.
     void ResponseRecieved(MediatorArg mediatorArg);	// This internal function handles maintenance when a response is recieved. Then the _callback is called.
-
+    unsigned long _timeoutSecs;			// How long before we stop waiting for a response
 };
 #endif // ASYNCMEDIATORWORKER_H
