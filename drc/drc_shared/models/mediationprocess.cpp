7504653181bb3e26238489ba6638923b927b69e5
Michael Degan <plithnar@gmail.com>
2014-06-30 12:07:55 -0700

    .
    Hey, let's have the updated time actually change... Here we go!

--- drc/drc_shared/models/mediationprocess.cpp
+++ drc/drc_shared/models/mediationprocess.cpp
@@ -66,8 +66,8 @@ QString MediationProcess::UpdateParse()
 {
     QString toUpdate = QString("DisputeType = %1, UpdatedDate = '%2', UpdatedDateTime = '%3', DisputeState = %4, DisputeCounty = %5, ")
             .arg(QString::number(this->GetDisputeType()))
-            .arg(this->GetUpdatedDate().toString("yyyy-MM-dd"))
-            .arg(this->GetUpdatedDate().toString("yyyy-MM-dd hh:mm:ss"))
+            .arg(QDateTime::currentDateTime().toString("yyyy-MM-dd"))
+            .arg(QDateTime::currentDateTime().toString("yyyy-MM-dd hh:mm:ss"))
             .arg(QString::number(this->GetCurrentState()))
             .arg(QString::number(this->GetCountyId()));
 
